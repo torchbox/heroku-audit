c9b89c7d2546ffb20c62fdd2f52e0ee4a2e52eae
Jake Howard <jake.howard@torchbox.com>
2023-08-04 14:34:58 +0100

    Add command to audit postgres plans

--- heroku_audit/cli/postgres.py
+++ heroku_audit/cli/postgres.py
@@ -4,6 +4,7 @@ from typing import Optional, Annotated, TypedDict
 from rich.progress import track
 from concurrent.futures import ThreadPoolExecutor
 from heroku_audit.format import display_data, FormatOption, Format
+from heroku3.models.addon import Addon
 
 app = typer.Typer()
 
@@ -29,11 +30,15 @@ def get_heroku_postgres_details(addon) -> HerokuPostgresDetails:
     return {"postgres_version": data["info"]["PG Version"][0]}
 
 
-def get_version_column(addon):
+def get_addon_plan(addon: Addon):
+    return addon.plan.name.removeprefix(HEROKU_POSTGRES)
+
+
+def get_version_column(addon: Addon):
     return {
         "App": addon.app.name,
         "Addon": addon.name,
-        "Plan": addon.plan.name.removeprefix(HEROKU_POSTGRES),
+        "Plan": get_addon_plan(addon),
         "Version": get_heroku_postgres_details(addon)["postgres_version"],
     }
 
@@ -49,11 +54,10 @@ def version(
     """
     Audit the available postgres database versions
     """
-    results = []
     with ThreadPoolExecutor() as executor:
         apps = heroku.apps()
-        collected_addons = []
 
+        collected_addons = []
         for addons in track(
             executor.map(lambda a: a.addons(), apps),
             description="Loading addons...",
@@ -63,6 +67,7 @@ def version(
                 addon for addon in addons if addon.plan.name.startswith(HEROKU_POSTGRES)
             )
 
+        results = []
         for result in track(
             executor.map(get_version_column, collected_addons),
             description="Probing databases...",
@@ -73,3 +78,49 @@ def version(
             results.append(result)
 
     display_data(sorted(results, key=lambda r: r["Version"]), format)
+
+
+@app.command()
+def plan(
+    target: Annotated[
+        Optional[str],
+        typer.Argument(help="Plan to look for"),
+    ] = None,
+    format: FormatOption = Format.TABLE,
+):
+    # HACK: https://github.com/martyzz1/heroku3.py/pull/132
+    Addon._strs.append("config_vars")
+
+    with ThreadPoolExecutor() as executor:
+        apps = heroku.apps()
+
+        collected_addons = []
+        for addons in track(
+            executor.map(lambda a: a.addons(), apps),
+            description="Loading addons...",
+            total=len(apps),
+        ):
+            collected_addons.extend(
+                addon for addon in addons if addon.plan.name.startswith(HEROKU_POSTGRES)
+            )
+
+    if target:
+        collected_addons = [
+            addon for addon in collected_addons if get_addon_plan(addon) == target
+        ]
+
+    display_data(
+        sorted(
+            (
+                {
+                    "App": addon.app.name,
+                    "Addon": addon.name,
+                    "Attachments": ", ".join(sorted(addon.config_vars)),
+                    "Plan": get_addon_plan(addon),
+                }
+                for addon in collected_addons
+            ),
+            key=lambda r: r["App"],
+        ),
+        format,
+    )
