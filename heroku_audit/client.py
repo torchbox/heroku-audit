8c34ce85743676c1610accb6154d5f7943960d78
Jake Howard <jake.howard@torchbox.com>
2023-08-16 16:27:43 +0100

    Add better message when no API key is set

--- heroku_audit/client.py
+++ heroku_audit/client.py
@@ -1,4 +1,10 @@
 import os
 import heroku3
+from rich.text import Text
+from rich import print
 
-heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
+try:
+    heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
+except KeyError:
+    print(Text("Please set $HEROKU_API_KEY to a valid Heroku API key.", style="red"))
+    exit(1)
