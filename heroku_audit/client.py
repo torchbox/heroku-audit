5af92d6d82abf57aed02f072e88c2b64bbebdaac
Jake Howard <jake.howard@torchbox.com>
2023-08-18 10:44:17 +0100

    Use `exit` from `sys` for pyinstaller support

--- heroku_audit/client.py
+++ heroku_audit/client.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import heroku3
 from rich import print
@@ -8,4 +9,4 @@ try:
     heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
 except KeyError:
     print(Text("Please set $HEROKU_API_KEY to a valid Heroku API key.", style="red"))
-    exit(1)
+    sys.exit(1)
