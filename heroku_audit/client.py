0f4e6bf65bba030a237485bad8c1c7eda7ac6e22
Jake Howard <jake.howard@torchbox.com>
2023-09-07 16:13:47 +0100

    Only require Heroku API key when accessing Heroku API

--- heroku_audit/client.py
+++ heroku_audit/client.py
@@ -1,12 +1,35 @@
 import os
 import sys
+from typing import Any, cast
 
 import heroku3
+from heroku3.core import Heroku
 from rich import print
 from rich.text import Text
 
-try:
-    heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
-except KeyError:
-    print(Text("Please set $HEROKU_API_KEY to a valid Heroku API key.", style="red"))
-    sys.exit(1)
+__all__ = ["heroku"]
+
+
+class LazyHerokuWrapper:
+    """
+    A lazy heroku wrapper which only requires an API key when it's used
+    """
+
+    _heroku = None
+
+    def __get__(self, obj: Any, objtype: Any = None) -> Heroku:
+        if self._heroku is None:
+            try:
+                self._heroku = heroku3.from_key(os.environ["HEROKU_API_KEY"])
+            except KeyError:
+                print(
+                    Text(
+                        "Please set $HEROKU_API_KEY to a valid Heroku API key.",
+                        style="red",
+                    )
+                )
+                sys.exit(1)
+        return self._heroku
+
+
+heroku = cast(Heroku, LazyHerokuWrapper())
--- stubs/heroku3/__init__.pyi
+++ stubs/heroku3/__init__.pyi
@@ -1,11 +1,3 @@
-from requests import Session
-
-from .models.app import App
-
-class Heroku:
-    _session: Session
-
-    def apps(self) -> list[App]: ...
-    def app(self, id_or_name: str) -> App: ...
+from .core import Heroku
 
 def from_key(api_key: str) -> Heroku: ...
